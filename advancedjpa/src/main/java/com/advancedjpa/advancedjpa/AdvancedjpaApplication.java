96d7335e183ab0836f4342f8b12ae732410d3000
Aakash Deogaonkar <deogaonkar.a@husky.neu.edu>
2020-01-23 19:19:01 -0500

    Advancedjpa project created

---
+++ advancedjpa/src/main/java/com/advancedjpa/advancedjpa/AdvancedjpaApplication.java
@@ -0,0 +1,13 @@
+package com.advancedjpa.advancedjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedjpaApplication.class, args);
+	}
+
+}
---
+++ advancedjpa/src/test/java/com/advancedjpa/advancedjpa/AdvancedjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advancedjpa.advancedjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
