c845ca5e1047fdaba2c89556c6b0ce38e1d34a7b
Aakash Deogaonkar <deogaonkar.a@husky.neu.edu>
2020-01-25 19:19:46 -0500

    Fetch Student with Passport using Eager

---
+++ advancedjpa/src/test/java/com/advancedjpa/advancedjpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.advancedjpa.advancedjpa.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.advancedjpa.advancedjpa.AdvancedjpaApplication;
+import com.advancedjpa.advancedjpa.entity.Course;
+import com.advancedjpa.advancedjpa.entity.Student;
+
+@SpringBootTest(classes=AdvancedjpaApplication.class)
+class StudentRepositoryTest {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	StudentRepository studentrepository;
+	
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void findByStudentandPassportTest() {
+		Student student = studentrepository.findById(20001);
+		logger.info("Student with ID 20001 -> {}", student);
+		logger.info("Passport Details of Student with ID 2001 -> {}",student.getPassport());
+	}
+}
