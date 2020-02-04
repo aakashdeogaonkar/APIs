709f999cfc1f0798822fae4a27c150acc026e358
Aakash Deogaonkar <deogaonkar.a@husky.neu.edu>
2020-02-04 16:05:13 -0500

    Test added for retrieving Course from Review and vice versa

--- advancedjpa/src/test/java/com/advancedjpa/advancedjpa/repository/CourseRepositoryTest.java
+++ advancedjpa/src/test/java/com/advancedjpa/advancedjpa/repository/CourseRepositoryTest.java
@@ -3,6 +3,9 @@ package com.advancedjpa.advancedjpa.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +16,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.advancedjpa.advancedjpa.AdvancedjpaApplication;
 import com.advancedjpa.advancedjpa.entity.Course;
+import com.advancedjpa.advancedjpa.entity.Review;
 
 @SpringBootTest(classes=AdvancedjpaApplication.class)
 class CourseRepositoryTest {
 	
 	@Autowired
 	CourseRepository repository;
+	
+	@Autowired
+	EntityManager em;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Test
 	void findByIdTest() {
@@ -44,6 +53,21 @@ class CourseRepositoryTest {
 		
 		Course course1 = repository.findById(1);
 		assertEquals("BA&I", course1.getName());
+	}
+	
+	@Test
+	@DirtiesContext
+	@Transactional
+	void retrieveReviewsForCourse() {
+		Course c1 = repository.findById(6225L);
+		logger.info("Reviews of course 6225 -> {}",c1.getReviews());
+	}
+	
+	@Test
+	@Transactional
+	public void retrieveCourseFromReview() {
+		Review r1 = em.find(Review.class, 50001L);
+		logger.info("Course for review with ID 50001 -> {}", r1.getCourse());
 	}
 
 }
